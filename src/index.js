192bebd18421bd6a596f93c81de80a63fa1cc4bd
undefcat <undefcat@gmail.com>
2021-10-13 01:43:09 +0900

    feat: fontawesome import

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { MemoryRouter as Router } from 'react-router-dom'
 import './index.css'
+import '@fortawesome/fontawesome-free/css/all.css'
 import App from './App'
 
 ReactDOM.render(
