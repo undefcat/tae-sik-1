4f00253a6044e2bc7b61f5a3bc1e646c08ca8097
undefcat <undefcat@gmail.com>
2021-10-13 01:57:14 +0900

    feat(redux): redux 기본 설정

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
 import { MemoryRouter as Router } from 'react-router-dom'
 import './index.css'
 import '@fortawesome/fontawesome-free/css/all.css'
 import App from './App'
+import store from './store'
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </Router>
   </React.StrictMode>,
   document.getElementById('root')
